0ccfd6b4b646597c8a4fd86fa06359bfb56f48be
kidusking-glich <abelyohannes69@gmail.com>
2025-11-02 21:49:04 +0300

    registering model and displaying

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date')
+    search_fields = ('title', 'author')
+    list_filter = ('author', 'publication_date',)
+    # Optional: Set the fields that appear as links to the change page
+    list_display_links = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
