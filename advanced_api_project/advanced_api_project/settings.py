7a15a3141fef8599ab750c1ce9634d45eaef12ff
kidusking-glich <abelyohannes69@gmail.com>
2025-11-29 22:00:33 +0300

    Creating api app and adding in the installed app

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     # Third-party apps
+    'rest_framework',
+
+    # Local app
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
