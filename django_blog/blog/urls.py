69926856eb72a7cd80559b7384407508a3db3fa0
kidusking-glich <abelyohannes69@gmail.com>
2025-12-06 23:24:56 +0300

    the URL configuration for the tagging feature is functionally correct.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -44,4 +44,6 @@ urlpatterns = [
     # Built-in Auth Views
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'), 
+    # CORRECT MAPPING:
+    path('tags/<slug:tag_slug>/', views.PostTagListView.as_view(), name='posts_by_tag'),
 ]
