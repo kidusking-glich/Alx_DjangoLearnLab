03834351cd395e354cacae61616a79ffe68f5841
kidusking-glich <abelyohannes69@gmail.com>
2025-12-08 19:45:27 +0300

    register the new apps and specify the custom user model.

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -31,14 +31,31 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Thired Party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # Local apps
+    'account'
 ]
+#Custom user model
+AUTH_USER_MODEL ='accounts.CustomUser'
 
+# Config REST Framework settings
+REST_FREAMWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
