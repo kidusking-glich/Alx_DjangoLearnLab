3cc57966a1c2b66284ad42116de3b86e6d776a30
kidusking-glich <abelyohannes69@gmail.com>
2025-12-11 20:12:39 +0300

    Fix

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,29 +3,52 @@ from .models import CustomUser
 from rest_framework.authtoken.models import Token
 
 
+# class CustomUserRegistrationSerializer(serializers.ModelSerializer):
+#     password =serializers.CharField(write_only=True)
+
+#     class Meta:
+#         model =CustomUser
+#         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'bio')
+
+#     def create(self, validated_data):
+#         # Retrieve the required 'password' field and hash it
+#         password = validated_data.pop('password')
+
+#         # Create the user using the remaining validated data
+#         user = CustomUser.objects.create_user(
+#             username= validated_data['username'],
+#             email = validated_data.get('email', ''),
+#             password=validated_data['password'],
+
+#             # Ensure all field retrievals use brackets or .get()
+#             first_name=validated_data.get('first_name', ''), 
+#             last_name=validated_data.get('last_name', ''), 
+#             bio=validated_data.get('bio', None)
+#         )
+#         return user 
+
+
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
-    password =serializers.CharField(write_only=True)
+    # This line MUST be present.
+    password = serializers.CharField(write_only=True) 
 
     class Meta:
-        model =CustomUser
+        model = CustomUser
+        # 'password' must be included in fields
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'bio')
 
     def create(self, validated_data):
-        # Retrieve the required 'password' field and hash it
-        password = validated_data.pop('password')
-
-        # Create the user using the remaining validated data
+        # We access the CustomUser model directly here
         user = CustomUser.objects.create_user(
-            username= validated_data['username'],
-            email = validated_data.get('email', ''),
-            password=validated_data['password'],
-
-            # Ensure all field retrievals use brackets or .get()
-            first_name=validated_data.get('first_name', ''), 
-            last_name=validated_data.get('last_name', ''), 
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'], # Accessing the validated, unhashed password
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
             bio=validated_data.get('bio', None)
         )
-        return user 
+        return user
+
     
 class CustomUserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField()
