04bba790838a52ffd274cebe72cd30c8a32ad099
kidusking-glich <abelyohannes69@gmail.com>
2025-12-11 20:16:43 +0300

    serializers.CharField(write_only=True) which is mandatory for handling the password securely during registration.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -28,42 +28,93 @@ from rest_framework.authtoken.models import Token
 #         return user 
 
 
+
+
+
+
+# Serializer for User Registration
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
-    # This line MUST be present.
+    # MANDATORY: Defines the 'password' field specifically as CharField
+    # 'write_only=True' ensures it's accepted on input but never returned on output.
     password = serializers.CharField(write_only=True) 
 
     class Meta:
         model = CustomUser
-        # 'password' must be included in fields
+        # 'password' must be included here to be accepted in the POST request
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'bio')
 
+    # Custom create method to properly hash and save the password
     def create(self, validated_data):
-        # We access the CustomUser model directly here
+        # The CustomUser manager handles create_user, hashing the password.
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password'], # Accessing the validated, unhashed password
+            password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
             bio=validated_data.get('bio', None)
         )
         return user
 
-    
+# Serializer for viewing and updating user profile
 class CustomUserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField()
     following_count = serializers.SerializerMethodField()
 
     class Meta:
-        model =CustomUser
+        model = CustomUser
         fields = (
-            'id', 'username', 'email', 'first_name', 'last_name','bio', 'prfile_picture',
-            'followers_count', 'following_count'
+            'id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture',
+            'date_joined', 'followers_count', 'following_count'
         )
-        read_only_fields = ('username', 'email', 'followers_count', 'following_count')
+        read_only_fields = ('username', 'email', 'date_joined', 'followers_count', 'following_count')
 
-        def get_followers_count(self, obj):
-            return obj.followers.count()
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
+
+
+
+
+# class CustomUserRegistrationSerializer(serializers.ModelSerializer):
+#     # This line MUST be present.
+#     password = serializers.CharField(write_only=True) 
+
+#     class Meta:
+#         model = CustomUser
+#         # 'password' must be included in fields
+#         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'bio')
+
+#     def create(self, validated_data):
+#         # We access the CustomUser model directly here
+#         user = CustomUser.objects.create_user(
+#             username=validated_data['username'],
+#             email=validated_data.get('email', ''),
+#             password=validated_data['password'], # Accessing the validated, unhashed password
+#             first_name=validated_data.get('first_name', ''),
+#             last_name=validated_data.get('last_name', ''),
+#             bio=validated_data.get('bio', None)
+#         )
+#         return user
+
+    
+# class CustomUserProfileSerializer(serializers.ModelSerializer):
+#     followers_count = serializers.SerializerMethodField()
+#     following_count = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model =CustomUser
+#         fields = (
+#             'id', 'username', 'email', 'first_name', 'last_name','bio', 'prfile_picture',
+#             'followers_count', 'following_count'
+#         )
+#         read_only_fields = ('username', 'email', 'followers_count', 'following_count')
+
+#         def get_followers_count(self, obj):
+#             return obj.followers.count()
         
-        def get_following_count(self, obj):
-            return obj.following.count()#'following' is the related_name
+#         def get_following_count(self, obj):
+#             return obj.following.count()#'following' is the related_name
